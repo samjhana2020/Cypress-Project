4a035fd4f7d9263bab4342f0f30575b10003a605
e25104 <brijesh.bisht@extramarks.com>
2023-03-26 12:45:51 +0530

    First project setup

---
+++ cypress/e2e/MyFirstTest.cy.js
@@ -0,0 +1,30 @@
+describe('My first Test Suite',()=>{
+
+    it('test1',()=>{
+   cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+   cy.title().should('eq', 'OrangeHRM')
+    })
+
+    it('test2',()=>{
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+       
+        cy.get('[name="username"]').type('Admin')
+        cy.get('[name="password"]').type('admin123')
+        cy.get('[type="submit"]').click()
+
+         })
+
+
+
+
+    // })
+
+    // it('test3',()=>{
+
+
+
+
+    // })
+
+
+})
